9eb95e517e6e554d8b7abfa2222b8f73b0d2275b
John Grey <johngrey4296@gmail.com>
2024-05-20 21:01:25 +0100

    [add]: add and use logger spec

--- jgdv/logging/log_config.py
+++ jgdv/logging/log_config.py
@@ -1,131 +1,143 @@
 #!/usr/bin/env python3
 """
+printing areas:
+
+[header]
+[setup]
+[cmd entry]
+[help]
+[plan]
+[task loop] - start
+
+[[Task Header]]:
+- [Action Group Running]
+-- [actions]
+-- [State]
+- [Task Queuing]
+- [Skips,Failure]
+- [Artifacts]
+[task loop] -exit
+
+[report]
+
+[shutdown]
 
 """
 
-##-- builtin imports
+# Imports:
 from __future__ import annotations
 
-# import abc
+# ##-- stdlib imports
 import datetime
 import enum
 import functools as ftz
 import itertools as itz
 import logging as logmod
+import os
 import pathlib as pl
 import re
 import time
 import types
 import weakref
-# from copy import deepcopy
-# from dataclasses import InitVar, dataclass, field
-from typing import (TYPE_CHECKING, Any, Callable, ClassVar, Final, Generic,
-                    Iterable, Iterator, Mapping, Match, MutableMapping,
-                    Protocol, Sequence, Tuple, TypeAlias, TypeGuard, TypeVar,
-                    cast, final, overload, runtime_checkable)
+from sys import stderr, stdout
+from typing import (TYPE_CHECKING, Any, Callable, ClassVar, Final, Generator,
+                    Generic, Iterable, Iterator, Mapping, Match,
+                    MutableMapping, Protocol, Sequence, Tuple, TypeAlias,
+                    TypeGuard, TypeVar, cast, final, overload,
+                    runtime_checkable)
 from uuid import UUID, uuid1
 
-##-- end builtin imports
+# ##-- end stdlib imports
+
+from jgdv.logging.log_colour import (JGDVColourFormatter,
+                                     JGDVColourStripFormatter)
+from tomlguard import TomlGuard
+from jgdv.logging.logger_spec import LoggerSpec
 
 ##-- logging
 logging = logmod.getLogger(__name__)
 ##-- end logging
 
-import os
-from sys import stdout, stderr
-from jgdv.logging.log_colour import JGDVColourFormatter, JGDVColourStripFormatter
-
 env : dict = os.environ
 
 class JGDVLogConfig:
-    """ Utility class to setup [stdout, stderr, file] logging. """
+    """ Utility class to setup [stdout, stderr, file] logging.
+      Also creates a 'printer' logger, so instead of using `print`,
+      tasks can notify the user using the printer,
+      which also includes the notifications into the general log trace
 
-    def __init__(self):
+      The Printer has a number of children, which can be controlled
+      to customise verbosity.
+
+      Standard _printer children:
+      [ action_exec, action_group, artifact, cmd, fail, header, help, queue,
+      report, skip, sleep, success, task, task_header, task_loop, task_state,
+      track,
+      ]
+
+    """
+
+    def __init__(self, printer:str="_printer"):
         # Root Logger for everything
         self.root    = logmod.root
+        self._printer_children = [
+            "action_exec", "action_group", "artifact", "cmd", "fail", "header", "help", "queue",
+            "report", "skip", "sleep", "success", "task", "task_header", "task_loop", "task_state",
+            "track",
+            ]
+        self.stream_initial_spec = LoggerSpec.build({"name": logmod.root.name,
+                                                     "level" : "WARNING",
+                                                     "target": "stdout",
+                                                     "format" : "{levelname}  : INIT : {message}",
+                                                    })
         # EXCEPT this, which replaces 'print(x)'
-        self.printer               = logmod.getLogger(doot.constants.printer.PRINTER_NAME)
+        self.printer_initial_spec = LoggerSpec.build({
+            "name": printer,
+            "level": "WARNING",
+            "target": "stdout",
+            "format": "{message}",
+            "propagate": False,
+            })
 
-        self.file_handler          = None
-        self.stream_handler        = logmod.StreamHandler(stdout)
-        self.print_stream_handler  = logmod.StreamHandler(stdout)
+        self.stream_initial_spec.apply()
+        self.printer_initial_spec.apply()
+        logging.debug("Post Log Setup")
 
-        self._pre_setup()
-
-    def _pre_setup(self):
-        """ a basic, config-less setup """
-        self.root.setLevel(logmod.NOTSET)
-        # self.file_handler.setFormatter(JGDVColourStripFormatter(fmt="{levelname} : INIT : {message}"))
-
-        self.stream_handler.setLevel(logmod.WARNING)
-        self.stream_handler.setFormatter(logmod.Formatter("{levelname}  : INIT : {message}", style="{"))
-
-        self.root.addHandler(self.stream_handler)
-
-        self.printer.propagate = False
-        self.print_stream_handler.setFormatter(logmod.Formatter("{message}", style="{"))
-        self.printer.setLevel(logmod.NOTSET)
-        self.printer.addHandler(self.print_stream_handler)
-        # self.printer.addHandler(self.file_handler)
-
-    def setup(self):
+    def setup(self, config:TomlGuard):
         """ a setup that uses config values """
-        assert(doot.config is not None)
-        self._setup_file_logging()
-        self._setup_stream_logging()
-        self._setup_print_logging()
+        assert(config is not None)
+        self.stream_initial_spec.clear()
+        self.printer_initial_spec.clear()
+        file_spec         = LoggerSpec.build(config.on_fail({}).logging.file(), name=LoggerSpec.RootName)
+        stream_spec       = LoggerSpec.build(config.on_fail({}).logging.stream(), name=LoggerSpec.RootName)
+        print_spec        = LoggerSpec.build(config.on_fail({}).logging.printer(), name="_printer")
+        file_spec.apply()
+        stream_spec.apply()
+        print_spec.apply()
+        self._setup_print_children(config)
+        self._setup_logging_extra(config)
 
-    def _setup_file_logging(self):
-        log_name_format = doot.config.on_fail("doot-%Y-%m-%d::%H:%M.log").logging.file.name()
-        log_file_name   = datetime.datetime.now().strftime(log_name_format)
+    def _setup_print_children(self, config):
+        basename      = "_printer"
+        subprint_data = config.on_fail({}).logging.subprinters()
+        for name in self._printer_children:
+            if name not in subprint_data:
+                continue
+            fullname = "{}.{}".format(basename, name)
+            match LoggerSpec.build(subprint_data[name], name=fullname):
+                case None:
+                    print("Could not build LoggerSpec for {}".format(name))
+                case LoggerSpec() as spec:
+                    spec.apply()
 
-        log_dir = doot.locs["{logs}"]
-        # TODO delete old logs if number of logs larger than N
-        if not log_dir.exists():
-            log_dir = pl.Path()
-
-        log_file_path = log_dir / log_file_name
-
-        self.file_handler  = logmod.FileHandler(log_file_path, mode='w')
-        file_log_level    = doot.config.on_fail("DEBUG", str|int).logging.file.level(wrapper=lambda x: logmod._nameToLevel.get(x, 0))
-        file_log_format   = doot.config.on_fail("{levelname} : {pathname} : {lineno} : {funcName} : {message}", str).logging.file.format()
-        file_filter_names = doot.config.on_fail([], list).logging.file.allow()
-
-        self.file_handler.setLevel(file_log_level)
-        self.file_handler.setFormatter(JGDVColourStripFormatter(fmt=file_log_format))
-        if bool(file_filter_names):
-            self.file_handler.addFilter(_JGDVAnyFilter(file_filter_names))
-
-        self.root.addHandler(self.file_handler)
-
-    def _setup_stream_logging(self):
-        stream_log_level    = doot.config.on_fail("WARNING", str|int).logging.stream.level(wrapper=lambda x: logmod._nameToLevel.get(x, 0))
-        stream_log_format   = doot.config.on_fail("{levelname} : {pathname} : {lineno} : {funcName} : {message}", str).logging.stream.format()
-        stream_filter_names = doot.config.on_fail([], list).logging.stream.allow()
-
-        self.stream_handler.setLevel(stream_log_level)
-        use_colour = doot.config.on_fail(False, bool).logging.stream.colour()
-        use_colour &= "PRE_COMMIT" not in env
-        if use_colour:
-            self.stream_handler.setFormatter(JGDVColourFormatter(fmt=stream_log_format))
-        else:
-            self.stream_handler.setFormatter(JGDVColourStripFormatter(fmt=stream_log_format))
-
-        if bool(stream_filter_names):
-            self.stream_handler.addFilter(_JGDVAnyFilter(stream_filter_names))
-
-    def _setup_print_logging(self):
-        printer_log_level    = doot.config.on_fail("NOTSET", str|int).logging.printer.level(wrapper=lambda x: logmod._nameToLevel.get(x, 0))
-        printer_log_format   = doot.config.on_fail("{message}", str).logging.printer.format()
-
-        self.print_stream_handler.setLevel(printer_log_level)
-
-        use_colour = doot.config.on_fail(False, bool).logging.printer.colour()
-        use_colour &= "PRE_COMMIT" not in env
-        if use_colour:
-            self.print_stream_handler.setFormatter(JGDVColourFormatter(fmt=printer_log_format))
-        else:
-            self.print_stream_handler.setFormatter(JGDVColourStripFormatter(fmt=printer_log_format))
-
-    def set_level(self, level):
-        self.stream_handler.setLevel(level)
+    def _setup_logging_extra(self, config):
+        """ read the config logging section
+          setting up each entry other than stream, file, and printer
+        """
+        extras = config.on_fail({}).logging.extra()
+        for key,data in extras.items():
+            match LoggerSpec.build(data, name=key):
+                case None:
+                    print("Could not build LoggerSpec for {}".format(name))
+                case LoggerSpec() as spec:
+                    spec.apply()
