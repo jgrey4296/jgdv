b56f9ea7520b1952f72ad0e4a673d8a761ae8a10
John Grey <johngrey4296@gmail.com>
2024-11-24 06:24:44 +0000

    [refactor]: remove mentions of doot from dkey

---
+++ jgdv/structs/dkey/errors.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+
+
+
+"""
+
+from __future__ import annotations
+
+import datetime
+import enum
+import functools as ftz
+import itertools as itz
+import logging as logmod
+import pathlib as pl
+import re
+import time
+import types
+import weakref
+from typing import (TYPE_CHECKING, Any, Callable, ClassVar, Final, Generator,
+                    Generic, Iterable, Iterator, Mapping, Match,
+                    MutableMapping, Protocol, Sequence, Tuple, TypeAlias,
+                    TypeGuard, TypeVar, cast, final, overload,
+                    runtime_checkable)
+from uuid import UUID, uuid1
+
+##-- logging
+logging = logmod.getLogger(__name__)
+##-- end logging
+
+class DKeyError(Exception):
+    pass
