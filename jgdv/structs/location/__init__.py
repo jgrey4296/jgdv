913fdb66a02d3db75de58fa7c79b3afd46f48488
John Grey <johngrey4296@gmail.com>
2024-11-26 20:57:14 +0000

    [chore]:

--- jgdv/structs/location/__init__.py
+++ jgdv/structs/location/__init__.py
@@ -1,4 +1,10 @@
+"""
+A module for working with locations.
+Provides two main classes:
+Location, like an extended pathlib.Path
+JGDVLocations, a mapping of names to locations
 
+"""
 from .location import LocationMeta_f, Location
 from .errors import LocationError, LocationExpansionError, DirAbsent
 from .locations import JGDVLocations
