ab6ba38f816d90c547f72d2a6bf7ef6115dc55cf
John Grey <johngrey4296@gmail.com>
2024-12-03 08:06:42 +0000

    [remove]: obsolete name module

--- jgdv/structs/name/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-A Module providing an extension to str,
-which parses and stores metadata out of the string.
-"""
-
-from .structured_name import StructuredName
-
-from .mixins import aware_splitter, TailEntry
